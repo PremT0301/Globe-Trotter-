e5dbd43e31afd59cea392d405122216d579b5d1f
jay sadhu <sadhujay2004@gmail.com>
2025-08-11 13:45:25 +0530

    login Page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import LandingPage from './pages/LandingPage';
 import { AuthProvider } from './context/AuthContext';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
@@ -11,10 +12,11 @@ function App() {
           <AnimatePresence mode="wait">
             <Routes>
               <Route path="/" element={<LandingPage />} />
+              <Route path="/login" element={<LoginPage />} />
             </Routes>
           </AnimatePresence>
         </div>
-      </Router>
+      </Router >
     </AuthProvider>
   );
 }
