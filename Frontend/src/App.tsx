15fc8ba2d9ce1f3515e9f5cde1f1c065fadbff40
PremT0301 <165881204+PremT0301@users.noreply.github.com>
2025-08-11 14:10:58 +0530

    Updated

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -4,7 +4,16 @@ import LandingPage from './pages/LandingPage';
 import { AuthProvider } from './context/AuthContext';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
+import AdminDashboard from './pages/AdminDashboard';
 import Navbar from './components/Navbar';
+import ItineraryBuilder from './pages/ItineraryBuilder';
+import TripBudget from './pages/TripBudget';
+import TripCalendar from './pages/TripCalendar';
+import CitySearch from './pages/CitySearch';
+import ActivitySearch from './pages/ActivitySearch';
+import CreateTrip from './pages/CreateTrip';
+import Dashboard from './pages/Dashboard';
+import MyTrips from './pages/MyTrips';
 
 function App() {
   return (
@@ -18,6 +27,14 @@ function App() {
               <Route path="/signup" element={<SignupPage />} />
               <Route path="/admin" element={<AdminDashboard />} />
               <Route path="/itinerary-builder" element={<ItineraryBuilder />} />
+              <Route path="/trip-budget" element={<TripBudget />} />
+              <Route path="/trip-calendar" element={<TripCalendar />} />
+              <Route path="/city-search" element={<CitySearch />} />
+              <Route path="/activity-search" element={<ActivitySearch />} />
+              <Route path="/create-trip" element={<CreateTrip />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/my-trips" element={<MyTrips />} />
+             
             </Routes>
           </AnimatePresence>
         </div>
